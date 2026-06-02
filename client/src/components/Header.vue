ba99d9fc25aa72ed1ab8fa41c28a72d1793c802c
Anna Khlystova <a.khlystova@emcd.io>
2026-06-02 11:53:53 +0300

    preparing publish client to GH pages

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="flex gap-4 items-center">
-    <a href="/"><img src="../assets/images/logo.svg" alt="Logo" /></a>
+    <a :href="baseUrl"><img src="../assets/images/logo.svg" alt="Logo" /></a>
   </div>
 </template>
+
+<script setup lang="ts">
+const baseUrl = import.meta.env.BASE_URL;
+</script>
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/challenge-weather-app/",
   resolve: {
     alias: {
       "@": new URL("./src", import.meta.url).pathname,
